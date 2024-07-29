0a66a4ed1e92a56771d08ce314e734650d766b2e
holaleche <whoisleche@gmail.com>
2024-07-29 14:12:50 +0900

    Chore 초기 빌드 생성 (Squash)
    Chore 초기 빌드 생성
    
    Chore 서버 연결 및 Socket I/O 설정 (백엔드)
    
    Chore MongoDB 서버 연결 (백엔드)
    
    Chore 주요 route 연결 (백엔드)
    
    Chore 리액트 파일 초기 설정 (프론트엔드)

---
+++ backend/app.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+require("./database/connection");
+
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
+
+const index = require("./routes/index");
+const users = require("./routes/users");
+const documents = require("./routes/documents");
+
+const app = express();
+
+app.use(cors());
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", index);
+app.use("/users", users);
+app.use("/documents", documents);
+
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
---
+++ backend/database/connection.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const DATABASE_URL = process.env.MONGOOSE_URL;
+
+const connectDB = async () => {
+  await mongoose.connect(DATABASE_URL);
+};
+
+module.exports = connectDB;
